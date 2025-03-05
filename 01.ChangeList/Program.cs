adc8378e84e5e0a91bd2fcae685415b157fdb19c
Teodor Kostov <148477548+rebelwithoutacause@users.noreply.github.com>
2025-03-05 08:55:21 +0200

    Add files via upload

---
+++ 01.ChangeList/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+         
+        List<int> numbers = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToList();
+
+        string input = Console.ReadLine();
+
+         
+        while (input != "end")
+        {
+            string[] cmdArg = input.Split(' ');
+            string command = cmdArg[0];
+
+            if (command == "Delete")
+            {
+                int elementToRemove = int.Parse(cmdArg[1]);
+                 
+                numbers.RemoveAll(x => x == elementToRemove);
+            }
+            else if (command == "Insert")
+            {
+                int element = int.Parse(cmdArg[1]);
+                int position = int.Parse(cmdArg[2]);
+                 
+                numbers.Insert(position, element);
+            }
+
+            
+            input = Console.ReadLine();
+        }
+
+         
+        Console.WriteLine(string.Join(" ", numbers));
+    }
+}
---
+++ 02.ListOfProducts/Program.cs
@@ -0,0 +1,35 @@
+﻿//Write a program that:
+//•	Read an integer number n and n lines of products
+//•	Print a numbered list of all the products ordered by name
+
+using System;
+
+namespace ProductList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int n = int.Parse(Console.ReadLine());
+
+            string[] products = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                products[i] = Console.ReadLine();
+            }
+
+
+            Array.Sort(products);
+
+
+           
+            for (int i = 0; i < products.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }
+        }
+    }
+}
